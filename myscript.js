83aea907af8233b135dab4670e246816474c6d0e
Janani-Ngii <janongii012@gmail.com>
2022-03-06 21:59:06 +0300

    adding the date form

---
+++ myscript.js
@@ -0,0 +1,18 @@
+var  month;
+if (Date=>1>=31) {
+  console.log(valid);
+} else {
+  console.log("enter valid date");
+}
+
+function akan(text){
+
+
+    if (day = sunday) console.log("Fizz");
+    
+          else if(number%5 === 0) console.log("Buzz");
+    
+          else if(number%3 === 0 && number %5 ===0) console.log("FizzBuzz");
+    
+          else console.log(number);
+    }
