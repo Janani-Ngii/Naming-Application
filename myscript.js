fd66e70f72ab591f603adcfdf9f9e873a9377468
Janani-Ngii <janongii012@gmail.com>
2022-03-06 23:03:44 +0300

    adding switch statement

--- myscript.js
+++ myscript.js
@@ -1,18 +1,55 @@
-var  month;
-if (Date=>1>=31) {
-  console.log(valid);
-} else {
-  console.log("enter valid date");
-}
-
-function akan(text){
-
-
-    if (day = sunday) console.log("Fizz");
-    
-          else if(number%5 === 0) console.log("Buzz");
-    
-          else if(number%3 === 0 && number %5 ===0) console.log("FizzBuzz");
-    
-          else console.log(number);
+function getName(){
+    var name = ""
+    var gender = ""
+    if (gender == "male"){
+    switch (new Date().getDay()) {
+        case "Sunday":
+          name = "Kwasi";
+          break;
+        case MOnday:
+          name = "Kwadwo";
+          break;
+        case Tuesday:
+           name = "Kwabena";
+          break;
+        case Wednesday:
+          name = "Kwaku";
+          break;
+        case Thursday:
+          name = "Yaw";
+          break;
+        case Friday:
+          name = "Kofi";
+          break;
+        case Saturday:
+          name = "Kwame";
+      }
     }
+
+    else if(gender ==  "female"){
+        switch (new Date().getDay()) {
+            case "Sunday":
+              name = "Akosua";
+              break;
+            case "Monday":
+              name = "Adwoa";
+              break;
+            case "Tuesday":
+                name = "Abenaa";
+              break;
+            case "Wednesday":
+                name = "Akua";
+              break;
+            case "Thursday":
+                name = "Yaa";
+              break;
+            case "Friday":
+                name = "Afua";
+              break;
+            case "Saturday":
+              name = "Ama";
+          }
+
+    }
+    else (alert("Input a proper gender"))
+}
